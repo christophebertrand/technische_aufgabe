6906e888025d418162b269fa51ca85338b71a258
christophe <christophe@linux.fritz.box>
2018-11-01 17:05:58 +0100

    Added description of blocking points

--- src/main/java/com/dlizarra/starter/purchase/Purchase.java
+++ src/main/java/com/dlizarra/starter/purchase/Purchase.java
@@ -1,7 +1,5 @@
 package com.dlizarra.starter.purchase;
 
-import com.dlizarra.starter.user.User;
-import com.dlizarra.starter.user.UserDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -26,12 +24,16 @@ public class Purchase {
     private String product;
 
     //@Column(nullable = false)
+    // I removed the constraint that the date cannot be null firstly to make it easier to test.
+    // And I got some mapping errors when I tried to use a date. (This is also the reason I added an
+    // @JsonIgnore to the creationTime and modificationTime in PurchaseDto.
     private LocalDateTime date;
 
     @Column(nullable = false)
     private int price;
 
-    //TODO find problem
+    // I tried to use the User class instead of a string but I got some runtime errors with
+    // the nested User class when the mapper tried to map from User to UserDto.
     //@JoinColumn(name = "user_id", nullable = false)
     //@ManyToOne(fetch = FetchType.LAZY)
     private String user;
